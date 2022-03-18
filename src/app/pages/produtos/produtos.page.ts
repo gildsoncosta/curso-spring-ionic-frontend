20c92bdff84263ea92900088a7d9e91344d0edb9
gildsoncosta <gildsoncosta@gmail.com>
2022-03-18 00:08:50 -0300

    Component loading

--- src/app/pages/produtos/produtos.page.ts
+++ src/app/pages/produtos/produtos.page.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable prefer-const */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable quote-props */
 /* eslint-disable @typescript-eslint/dot-notation */
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { API_CONFIG } from 'src/config/api.config';
 import { ProdutoDTO } from 'src/models/produto.dto';
 import { ProdutoService } from 'src/services/domain/produto.service';
@@ -21,17 +24,23 @@ export class ProdutosPage implements OnInit {
 
   private categoria_id: string;
 
-  constructor(private route: Router, public produtoService: ProdutoService, private _router: ActivatedRoute) { }
+  constructor(
+    private route: Router,
+    public produtoService: ProdutoService,
+    private _router: ActivatedRoute,
+    public loadingController: LoadingController) { }
 
   ngOnInit() {
   }
 
   ionViewDidEnter() {
+    let loader = this.presentLoading();
     this.categoria_id = this._router.snapshot.paramMap.get('categoria_id');
     //console.log('this._router.snapshot.paramMap.get: ', this.categoria_id);
     this.produtoService.findByCategoria(this.categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        //this.loadingController.dismiss();
         this.loadImageUrls();
       },
         error => { this.route.navigateByUrl('categorias'); });
@@ -51,5 +60,17 @@ export class ProdutosPage implements OnInit {
 
   showDetail(produtoId: string) {
     this.route.navigate(['produto-detail', {produto_id: produtoId}]);
+  }
+
+  async presentLoading() {
+    const loader = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Aguarde ...',
+      duration: 1500
+    });
+    await loader.present();
+    const { role, data } = await loader.onDidDismiss();
+    //console.log('Loading dismissed!');
+    return loader;
   }
 }
